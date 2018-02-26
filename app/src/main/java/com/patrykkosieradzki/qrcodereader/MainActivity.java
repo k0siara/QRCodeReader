49285a7f89cb553b434f227d6790f31da9ae3b97
k0siara <patryk.kosieradzki@gmail.com>
2018-02-26 19:40:40 +0100

    updated main activity

--- app/src/main/java/com/patrykkosieradzki/qrcodereader/MainActivity.java
+++ app/src/main/java/com/patrykkosieradzki/qrcodereader/MainActivity.java
@@ -1,10 +1,12 @@
 package com.patrykkosieradzki.qrcodereader;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +18,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivityForResult(new Intent(MainActivity.this, QRActivity.class), QR_READ);
@@ -30,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (resultCode == QR_READ) {
-            TextView text = (TextView) findViewById(R.id.text);
-
-            text.setText(data.getExtras().get("data").toString());
+            Snackbar.make(findViewById(R.id.coordinatorLayout), data.getExtras().get("data").toString(), Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
         }
     }
+
 }
