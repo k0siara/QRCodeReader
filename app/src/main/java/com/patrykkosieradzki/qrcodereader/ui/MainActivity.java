a1ddcd84613fac64f376b409d714b05d13cbc4d3
k0siara <patryk.kosieradzki@gmail.com>
2018-02-27 00:38:37 +0100

    added google signin

--- app/src/main/java/com/patrykkosieradzki/qrcodereader/ui/MainActivity.java
+++ app/src/main/java/com/patrykkosieradzki/qrcodereader/ui/MainActivity.java
@@ -2,22 +2,38 @@ package com.patrykkosieradzki.qrcodereader.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.patrykkosieradzki.qrcodereader.R;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int QR_READ = 0;
 
+    private GoogleSignInClient mGoogleSignInClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivityForResult(new Intent(MainActivity.this, QRActivity.class), QR_READ);
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                //startActivityForResult(new Intent(MainActivity.this, QRActivity.class), QR_READ);
             }
         });
     }
@@ -39,6 +56,49 @@ public class MainActivity extends AppCompatActivity {
             Snackbar.make(findViewById(R.id.coordinatorLayout), text, Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_how_it_works:
+                // TODO: Handle walkthrough
+                return true;
+
+            case R.id.action_settings:
+                // TODO: Handle settings
+                return true;
+
+            case R.id.action_logout:
+                // TODO: Handle logout
+                logout();
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logout() {
+        if (GoogleSignIn.getLastSignedInAccount(this) != null) {
+            mGoogleSignInClient.signOut()
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                        }
+                    });
+        } else {
+            // TODO: Handle user without google login
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
 }
