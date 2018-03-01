fa97a9d72aecedc9a7114334289d4647a37a3b49
k0siara <patryk.kosieradzki@gmail.com>
2018-03-01 16:46:10 +0100

    firebase login update

--- app/src/main/java/com/patrykkosieradzki/qrcodereader/ui/LoginActivity.java
+++ app/src/main/java/com/patrykkosieradzki/qrcodereader/ui/LoginActivity.java
@@ -3,18 +3,22 @@ package com.patrykkosieradzki.qrcodereader.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.Task;
 import com.patrykkosieradzki.qrcodereader.R;
 
@@ -22,12 +26,12 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = "LoginActivity";
     private static final int RC_SIGN_IN = 0;
 
-    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +43,16 @@ public class LoginActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .build();
+
     }
 
     @OnClick(R.id.signInButton)
     public void onSignInButtonClick() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
@@ -73,13 +81,16 @@ public class LoginActivity extends AppCompatActivity {
 
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
-            SharedPreferences sharedPref = getSharedPreferences("login", Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPref.edit();
-            editor.putInt("first_run", 0);
-            editor.apply();
+            if (completedTask.isSuccessful()) {
+                SharedPreferences sharedPref = getSharedPreferences("login", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putInt("first_run", 0);
+                editor.apply();
 
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            updateUI(account);
+                GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+                updateUI(account);
+            }
+
 
         } catch (ApiException e) {
             // The ApiException status code indicates the detailed failure reason.
@@ -92,5 +103,10 @@ public class LoginActivity extends AppCompatActivity {
     private void updateUI(GoogleSignInAccount account) {
         startActivity(new Intent(LoginActivity.this, HomeActivity.class));
         finish();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
