e0573497d69a9a2e4c771519ca287582b7a6ea70
k0siara <patryk.kosieradzki@gmail.com>
2018-03-02 19:44:22 +0100

    added firebase google auth

--- app/src/main/java/com/patrykkosieradzki/qrcodereader/application/QRCodeReaderApplication.java
+++ app/src/main/java/com/patrykkosieradzki/qrcodereader/application/QRCodeReaderApplication.java
@@ -2,6 +2,7 @@ package com.patrykkosieradzki.qrcodereader.application;
 
 import android.app.Application;
 
+import com.google.firebase.FirebaseApp;
 import com.squareup.leakcanary.LeakCanary;
 
 public class QRCodeReaderApplication extends Application {
@@ -10,6 +11,7 @@ public class QRCodeReaderApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        FirebaseApp.initializeApp(this);
         setupLeakCanary();
     }
 
--- app/src/main/java/com/patrykkosieradzki/qrcodereader/ui/LoginActivity.java
+++ app/src/main/java/com/patrykkosieradzki/qrcodereader/ui/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
@@ -19,7 +20,14 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.patrykkosieradzki.qrcodereader.R;
 
 import butterknife.BindView;
@@ -32,6 +40,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     private static final int RC_SIGN_IN = 0;
 
     private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,11 +49,12 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         ButterKnife.bind(this);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @OnClick(R.id.signInButton)
@@ -67,37 +77,55 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            if (completedTask.isSuccessful()) {
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+
                 SharedPreferences sharedPref = getSharedPreferences("login", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putInt("first_run", 0);
                 editor.apply();
 
-                GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-                updateUI(account);
+            } catch (ApiException e) {
+                // The ApiException status code indicates the detailed failure reason.
+                // Please refer to the GoogleSignInStatusCodes class reference for more information.
+                Log.w(TAG, "Google sign in failed", e);
             }
-
-
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            updateUI(null);
         }
     }
 
-    private void updateUI(GoogleSignInAccount account) {
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+    }
+
+    private void firebaseAnonymousAuth() {
+
+    }
+
+    private void updateUI(FirebaseUser user) {
         startActivity(new Intent(LoginActivity.this, HomeActivity.class));
         finish();
     }
