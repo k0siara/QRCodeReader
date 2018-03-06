16f3679851f00e76ddaf9ddf828ef2e67050fdd5
k0siara <patryk.kosieradzki@gmail.com>
2018-03-06 13:48:05 +0100

    removed unnecessary properties from ViewHolder, updated Adapter

--- app/src/main/java/com/patrykkosieradzki/qrcodereader/FirebaseBarcodeRecyclerAdapter.kt
+++ app/src/main/java/com/patrykkosieradzki/qrcodereader/FirebaseBarcodeRecyclerAdapter.kt
@@ -1,17 +1,18 @@
 package com.patrykkosieradzki.qrcodereader
 
 import android.content.Context
-import android.content.Intent
 import android.util.SparseBooleanArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.patrykkosieradzki.qrcodereader.model.QRCode
-import com.patrykkosieradzki.qrcodereader.ui.details.DetailsActivity
 import com.patrykkosieradzki.qrcodereader.animator.FlipAnimator
+import kotlinx.android.synthetic.main.row_item.view.*
 
 class FirebaseBarcodeRecyclerAdapter(options: FirebaseRecyclerOptions<QRCode>, mContext: Context)
     : FirebaseRecyclerAdapter<QRCode, ViewHolder>(options) {
@@ -31,43 +32,30 @@ class FirebaseBarcodeRecyclerAdapter(options: FirebaseRecyclerOptions<QRCode>, m
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int, model: QRCode) {
+        holder.bind(model)
 
-        holder.setTypeIcon(model.type)
-        holder.mTitle.text = model.text
-        holder.mDescription.text = model.type
-        holder.mText.text = model.text
 
         holder.itemView.isActivated = selectedItems.get(position, false)
 
 
-        // TODO: remove in future
-        holder.mMessageContainer.setOnClickListener { view ->
-            val intent = Intent(view.context, DetailsActivity::class.java)
-            intent.putExtra("text", model.text)
-            intent.putExtra("type", model.type)
-            view.context.startActivity(intent)
-        }
-
         applyClickEvents(holder, model, position)
-
-
         applyIconAnimation(holder, position)
 
 
     }
 
     private fun applyClickEvents(holder: ViewHolder, model: QRCode, position: Int) {
-        holder.iconContainer.setOnClickListener({
+        holder.itemView.icon_container.setOnClickListener({
             mFirebaseBarcodeRecyclerAdapterListener.onIconClicked(model, position)
         })
 
         //holder.iconImp.setOnClickListener(View.OnClickListener { listener.onIconImportantClicked(position) })
 
-        holder.mMessageContainer.setOnClickListener({
+        holder.itemView.content_container.setOnClickListener({
             mFirebaseBarcodeRecyclerAdapterListener.onMessageRowClicked(model, position)
         })
 
-        holder.mMessageContainer.setOnLongClickListener({
+        holder.itemView.content_container.setOnLongClickListener({
             mFirebaseBarcodeRecyclerAdapterListener.onRowLongClicked(model, position)
             true
         })
@@ -87,25 +75,28 @@ class FirebaseBarcodeRecyclerAdapter(options: FirebaseRecyclerOptions<QRCode>, m
     }
 
     private fun isItemSelected(position: Int): Boolean = selectedItems.get(position, false)
-    public fun isSelection(): Boolean = selectedItems.size() > 0
+    fun isSelection(): Boolean = selectedItems.size() > 0
 
     private fun applyIconAnimation(holder: ViewHolder, position: Int) {
+        val front: RelativeLayout = holder.itemView.icon_front
+        val back: RelativeLayout = holder.itemView.icon_back
+
         if (selectedItems.get(position, false)) {
-            holder.iconFront.visibility = View.GONE
-            resetIconYAxis(holder.iconBack)
-            holder.iconBack.visibility = View.VISIBLE
-            holder.iconBack.alpha = 1f
+            front.visibility = View.GONE
+            resetIconYAxis(back)
+            back.visibility = View.VISIBLE
+            back.alpha = 1f
             if (currentSelectedIndex === position) {
-                FlipAnimator.flipView(mContext, holder.iconBack, holder.iconFront, true)
+                FlipAnimator.flipView(mContext, back, front, true)
                 resetCurrentIndex()
             }
         } else {
-            holder.iconBack.visibility = View.GONE
-            resetIconYAxis(holder.iconFront)
-            holder.iconFront.visibility = View.VISIBLE
-            holder.iconFront.alpha = 1f
+            back.visibility = View.GONE
+            resetIconYAxis(front)
+            front.visibility = View.VISIBLE
+            front.alpha = 1f
             if (reverseAllAnimations && animationItemsIndex.get(position, false) || currentSelectedIndex === position) {
-                FlipAnimator.flipView(mContext, holder.iconBack, holder.iconFront, false)
+                FlipAnimator.flipView(mContext, back, front, false)
                 resetCurrentIndex()
             }
         }
--- app/src/main/java/com/patrykkosieradzki/qrcodereader/ViewHolder.kt
+++ app/src/main/java/com/patrykkosieradzki/qrcodereader/ViewHolder.kt
@@ -3,41 +3,38 @@ package com.patrykkosieradzki.qrcodereader
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.RelativeLayout
-import android.widget.TextView
+import com.patrykkosieradzki.qrcodereader.model.QRCode
+import kotlinx.android.synthetic.main.row_item.view.*
 
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    var mMessageContainer: LinearLayout = view.findViewById(R.id.message_container)
-    var iconContainer: RelativeLayout = view.findViewById(R.id.icon_container)
+    fun bind(qrCode: QRCode) {
+        itemView.title.text = qrCode.text
+        itemView.description.text = qrCode.text
+        itemView.secondary_description.text = qrCode.type
 
-    var mIcon: ImageView = view.findViewById(R.id.icon_profile)
-    var mTitle: TextView = view.findViewById(R.id.from)
-    var mDescription: TextView = view.findViewById(R.id.txt_primary)
-    var mText: TextView = view.findViewById(R.id.txt_secondary)
+        setTypeIcon(qrCode.type)
+    }
 
-    var iconBack: RelativeLayout = view.findViewById(R.id.icon_back)
-    var iconFront: RelativeLayout = view.findViewById(R.id.icon_front)
-
-
-    fun setTypeIcon(type: String) {
+    private fun setTypeIcon(type: String) {
         when (type) {
-            "ADDRESSBOOK" -> mIcon.setImageResource(R.drawable.ic_person_black_24dp)
-            "EMAIL_ADDRESS" -> mIcon.setImageResource(R.drawable.ic_email_black_24dp)
-            "PRODUCT" -> mIcon.setImageResource(R.drawable.ic_attach_money_black_24dp)
-            "URI" -> mIcon.setImageResource(R.drawable.ic_website_black_24dp)
-            "TEXT" -> mIcon.setImageResource(R.drawable.ic_text_fields_black_24dp)
-            "GEO" -> mIcon.setImageResource(R.drawable.ic_location_on_black_24dp)
-            "TEL" -> mIcon.setImageResource(R.drawable.ic_phone_black_24dp)
-            "SMS" -> mIcon.setImageResource(R.drawable.ic_sms_black_24dp)
-            "CALENDAR" -> mIcon.setImageResource(R.drawable.ic_date_range_black_24dp)
-            "WIFI" -> mIcon.setImageResource(R.drawable.ic_network_wifi_black_24dp)
-            "ISBN" -> mIcon.setImageResource(R.drawable.ic_library_books_black_24dp)
-            "VIN" -> mIcon.setImageResource(R.drawable.ic_directions_car_black_24dp)
+            "ADDRESSBOOK" -> setImage(R.drawable.ic_person_black_24dp)
+            "EMAIL_ADDRESS" -> setImage(R.drawable.ic_email_black_24dp)
+            "PRODUCT" -> setImage(R.drawable.ic_attach_money_black_24dp)
+            "URI" -> setImage(R.drawable.ic_website_black_24dp)
+            "TEXT" -> setImage(R.drawable.ic_text_fields_black_24dp)
+            "GEO" -> setImage(R.drawable.ic_location_on_black_24dp)
+            "TEL" -> setImage(R.drawable.ic_phone_black_24dp)
+            "SMS" -> setImage(R.drawable.ic_sms_black_24dp)
+            "CALENDAR" -> setImage(R.drawable.ic_date_range_black_24dp)
+            "WIFI" -> setImage(R.drawable.ic_network_wifi_black_24dp)
+            "ISBN" -> setImage(R.drawable.ic_library_books_black_24dp)
+            "VIN" -> setImage(R.drawable.ic_directions_car_black_24dp)
         }
+    }
 
-
+    private fun setImage(drawable: Int, imageView: ImageView = itemView.icon_image) {
+        imageView.setImageResource(drawable)
     }
 
 }
